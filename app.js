0a780f0e1a4fc5ba7936a459e9dde44d74f0e383
Misash <misash@outlook.com>
2022-02-12 21:56:45 -0500

    mailchimp API added

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+
+const app = express()
+
+//static file (css / img)
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//mailchimp API
+mailchimp.setConfig({
+    apiKey: "60ddf32ea34facd0fcba20dbda6c0cb3-us14",
+    server: "us14",
+});
+
+
+// ROUTES 
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+
+app.post("/", (req, res) => {
+
+    const lastName = req.body.lastName
+    const firstName = req.body.firstName
+    const email = req.body.email
+
+
+    const run = async () => {
+        const response = await mailchimp.lists.addListMember("24f08cd023", {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName ,
+                LNAME: lastName
+            }
+        });
+        console.log(response);
+    };
+    
+    run();
+    
+})
+
+
+
+
+
+// KEY 60ddf32ea34facd0fcba20dbda6c0cb3-us14
+
+// list id 24f08cd023
+
+// https://<dc>.api.mailchimp.com/3.0/
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+}) 
